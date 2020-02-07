cda46cd7bf8f1bea7dea4314c87dda960e49b295
Pierce Alworth <pbalworth@gmail.com>
2020-02-07 11:27:52 -0700

    Final Commit. Stuck on User story 3 but test and limited functionality added.

--- spec/features/projects/user_can_see_single_project_spec.rb
+++ spec/features/projects/user_can_see_single_project_spec.rb
@@ -9,12 +9,25 @@ RSpec.describe 'As a visitor', type: :feature do
                               material: 'Lamp Shade')
 
 
-      visit "/projects/#{challenge_1.projects.first.id}"
-
+  visit "/projects/#{litfit.id}"
 
       expect(page).to have_content(litfit.name)
       expect(page).to have_content("Challenge Theme: #{challenge_1.theme}")
       expect(page).to have_content("Material: #{litfit.material}")
+    end
+
+    it "shows me the number of contestants on the project" do
+      challenge_1 = Challenge.create!(theme: 'Apartment Furnishings', project_budget: 1000)
+
+      litfit = challenge_1.projects.create!(name: 'Litfit',
+                              material: 'Lamp Shade')
+
+      contestant_1 = Contestant.create!(name: "Pete", age: 30, hometown: "Medina, MN",
+                                                                years_of_experience: 4)
+      litfit.contestants << contestant_1
+
+      visit "/projects/#{litfit.id}"
+      expect(page).to have_content("Number of Contestants: 1")
     end
   end
 end
